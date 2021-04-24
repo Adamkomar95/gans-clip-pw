a2fe7365a59f57fffd546668780cbbf9196aa67c
maciejdomagala <mdomagala@7bulls.com>
2021-04-24 17:00:57 +0200

    Change .gitignore

--- setup.py
+++ setup.py
@@ -1,28 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pw-gans-clip',
+    name='taming-transformers',
+    version='0.0.1',
+    description='Taming Transformers for High-Resolution Image Synthesis',
     packages=find_packages(),
-    include_package_data=True,
-    version='0.0.0',
-    license='MIT',
-    description='PW-GANS-CLIP',
-    author='Adam Komorowski, Maciej Domagała',
-    keywords=[
-        'artificial intelligence',
-        'deep learning',
-        'transformers',
-        'text to image',
-        'generative adversarial networks'
-    ],
     install_requires=[
-        'numpy'
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
+        'torch',
+        'numpy',
+        'tqdm',
     ],
 )
