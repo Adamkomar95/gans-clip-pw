c80aaaeef42665cf35fa699a90ae26ccd78e7452
maciejdomagala <mdomagala80@gmail.com>
2021-04-27 00:31:01 +0200

    git account test

--- taming/models/vqgan.py
+++ taming/models/vqgan.py
@@ -20,6 +20,7 @@ def instantiate_from_config(config):
         raise KeyError("Expected key `target` to instantiate.")
     return get_obj_from_str(config["target"])(**config.get("params", dict()))
     
+    
 
 class VQModel(pl.LightningModule):
     def __init__(self,
